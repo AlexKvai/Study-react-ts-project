1b0da81b75e5465a279678f53832fa9fa4a1139e
alex parfenov <alexp-2004@mail.ru>
2022-08-10 21:16:05 +0700

    add dynamic class on button

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { IProduct } from '../models';
 
 interface ProductProps {
@@ -5,12 +6,27 @@ interface ProductProps {
 }
 
 export function Product({ product }: ProductProps) {
+  const [details, setDetails] = useState(false);
+  const btnClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+  const btnClasses = ['py-2 px-4 border', btnClassName];
+
   return (
     <div className='border py-2 px-4 flex flex-col items-center mb-2'>
       <img src={product.image} alt={product.title} className='w-1/6' />
       <p>{product.title}</p>
       <p className='font-bold'> {product.price}</p>
-      {/* <p>{product.description}</p> */}
+      <button
+        className={btnClasses.join(' ')}
+        onClick={() => setDetails((prevState) => !prevState)}
+      >
+        {details ? 'Hide details ' : 'Show details'}
+      </button>
+
+      {details && (
+        <div>
+          <p>{product.description}</p>
+        </div>
+      )}
     </div>
   );
 }
